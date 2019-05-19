612980dbc8745c893bcce30cf14e628c6f9cc7b9
Yuske Goto <yuskegoto@gmail.com>
2019-05-19 22:39:37 +0900

    2nd attempt to load svg2gcode

--- workspace.js
+++ workspace.js
@@ -351,8 +351,8 @@ cpdefine("inline:com-chilipeppr-workspace-yg", ["chilipeppr_ready"], function() 
             this.svg2gcodeObj = function() {
                 return {
                     name: "svg2gcode",
-                     url: "http://raw.githubusercontent.com/chilipeppr/widget-svg2gcode/master/auto-generated-widget.html",
-                    // url: "https://yuskegoto.github.io/widget-svg2gcode-yg/auto-generated-widget.html",
+                    //  url: "http://raw.githubusercontent.com/chilipeppr/widget-svg2gcode/master/auto-generated-widget.html",
+                    url: "https://yuskegoto.github.io/widget-svg2gcode-yg/auto-generated-widget.html",
                     id: "com-zipwhip-widget-svg2gcode-yg",
                     btn: $('#com-chilipeppr-ws-menu .svg2gcode-button'),
                     div: $('#com-chilipeppr-ws-svg2gcode-yg'),
